25e7701b5054c8b6a659cc474b4b77c729f5cb63
stanicarcaleb <78245208+Caleb-Stanicar@users.noreply.github.com>
2023-02-15 23:37:57 -0500

    finishing chat features

--- app/src/main/java/com/example/booktips/ChatActivity.kt
+++ app/src/main/java/com/example/booktips/ChatActivity.kt
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class ChatActivity : AppCompatActivity() {
     private lateinit var chatRecyclerView: RecyclerView
@@ -14,6 +16,7 @@ class ChatActivity : AppCompatActivity() {
     private lateinit var sendButton: ImageView
     private lateinit var messageAdapter: MessageAdapter
     private lateinit var messageList: ArrayList<Message>
+    private lateinit var mDbRef: DatabaseReference
 
     var receiverRoom: String? = null
     var senderRoom: String? = null
@@ -26,6 +29,7 @@ class ChatActivity : AppCompatActivity() {
         val receiverUid = intent.getStringExtra("uid")
 
         val senderUid = FirebaseAuth.getInstance().currentUser?.uid
+        mDbRef = FirebaseDatabase.getInstance().getReference()
 
         senderRoom = receiverUid + senderUid
         receiverRoom = senderUid + receiverUid
@@ -38,8 +42,32 @@ class ChatActivity : AppCompatActivity() {
         messageList = ArrayList()
         messageAdapter = MessageAdapter(this,messageList)
 
+        chatRecyclerView.layoutManager = LinearLayoutManager(this)
+        chatRecyclerView.adapter = messageAdapter
+
+        mDbRef.child("chats").child(senderRoom!!).child("messages").addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                messageList.clear()
+                for(postSnapshot in snapshot.children){
+                    val message = postSnapshot.getValue(Message::class.java)
+                    messageList.add(message!!)
+                }
+                messageAdapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+
         sendButton.setOnClickListener {
             val message = messageBox.text.toString()
+            val messageObject = Message(message,senderUid)
+
+            mDbRef.child("chats").child(senderRoom!!).child("messages").push().setValue(messageObject).addOnSuccessListener {
+                mDbRef.child("chats").child(receiverRoom!!).child("messages").push().setValue(messageObject)
+            }
+            messageBox.setText("")
         }
     }
 }
